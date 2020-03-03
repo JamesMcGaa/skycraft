c78ff4805fd0a1ad3f07a272a682539aeda48058
Karthik Nair <psionicpawn@gmail.com>
2020-03-03 16:39:19 -0500

    Added basic enemy logic
    First time pls be gentle

---
+++ Assets/Prefabs/enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int enemyType;
+    public Sprite sprite;
+
+    public int pathType;
+    public Vector3[] waypoints;
+    
+    Dictionary<string, int> stats; //we need hp, attack rate, damage, speed
+
+    private int curr_pt = 1;
+    public enemy(int ty, int pathty){
+        enemyType = ty;
+        pathType = pathty;
+        //everything should be loaded based off the type
+    }
+    //use this only for defining enemy archetypes
+    public enemy(Sprite spr, Dictionary<string, int> sts){
+        sprite = spr;
+        stats = sts;
+    }
+
+    void Start()
+    {
+        enemy archetype = enemy_controller.enemyTypeDict[enemyType];
+        sprite = archetype.sprite;
+        stats = archetype.stats;
+        print(stats["hp"]);
+        GetComponent<SpriteRenderer>().sprite = sprite;
+        waypoints = enemy_controller.pathTypeDict[pathType];
+        for(int i=0; i < waypoints.Length; i++){
+            waypoints[i] = enemy_controller.processWaypt(waypoints[i]);
+        }
+        transform.position = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //have we reached the next checkpoint?
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[curr_pt], stats["spd"] * Time.deltaTime);
+        if(curr_pt < waypoints.Length - 1 && Vector3.Distance(transform.position, waypoints[curr_pt]) < 1e-2){
+            curr_pt++;
+        }
+    }
+}
---
+++ Assets/Prefabs/enemy_controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static Dictionary<int, enemy> enemyTypeDict;
+    public static Dictionary<int, Vector3[]> pathTypeDict;
+
+
+    void Awake(){
+    	enemyTypeDict = new Dictionary<int, enemy>();
+        pathTypeDict = new Dictionary<int, Vector3[]>();
+
+
+        //ENEMY 1///////////////////////
+        enemyTypeDict.Add(0, new enemy(
+        	Resources.Load<Sprite>("enemy8"),
+        	new Dictionary<string, int> {{"hp",10}, {"ar",1}, {"dmg",1}, {"spd",3}})
+        );
+        
+
+        pathTypeDict.Add(0, new Vector3[]{
+    		new Vector3(0,.9f,0),
+    		new Vector3(.9f,.9f,0),
+            new Vector3(0f,0f,0)
+        });
+        ///////////////////////////////
+    }
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static Vector3 processWaypt(Vector3 pt){
+        Vector3 stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
+        return new Vector3((2 * pt[0] - 1) * stageDimensions[0], (2 * pt[1] - 1) * stageDimensions[1], 0);
+    }
+}
