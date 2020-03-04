0c210e4d5c75e9e9a4ee95e7d1d92cb19fe96bfd
mitadm <mitadm@FVFXN08NJ1WV.tld>
2020-03-03 19:33:42 -0500

    shotty

--- Assets/tower_controller.cs
+++ Assets/tower_controller.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TOWER_TYPE {
+  BASIC,
+  SHOTGUN,
+  SNIPER
+};
+
 public class tower_controller : MonoBehaviour
 {
 
     public GameObject bullet;
-    public float next_fire = -1f;
+    public TOWER_TYPE type;
     public float fire_rate = .1f;
 
     // Start is called before the first frame update
@@ -23,10 +29,26 @@ public class tower_controller : MonoBehaviour
       // }
     }
 
-    void Shoot(){
+    void Shoot() {
       Vector3 bullet_pos = transform.position;
-      bullet_pos.y += .5f;
-      Instantiate(bullet, bullet_pos, Quaternion.identity);
+      switch (type) {
+        case TOWER_TYPE.BASIC:
+          bullet_pos.y += .5f;
+          Instantiate(bullet, bullet_pos, Quaternion.identity);
+          break;
+        case TOWER_TYPE.SHOTGUN:
+          bullet_pos.y += .5f;
+          GameObject bullet1 = Instantiate(bullet, bullet_pos, Quaternion.identity);
+          bullet1.GetComponent<bullet_controller>().bullet_velocity = new Vector3(-0.01f, 0.1f, 0f);
+          GameObject bullet2 = Instantiate(bullet, bullet_pos, Quaternion.identity);
+          bullet2.GetComponent<bullet_controller>().bullet_velocity = new Vector3(0f, 0.1f, 0f);
+          GameObject bullet3 = Instantiate(bullet, bullet_pos, Quaternion.identity);
+          bullet3.GetComponent<bullet_controller>().bullet_velocity = new Vector3(0.01f, 0.1f, 0f);
+          break;
+        default:
+          break;
+      }
+
       Invoke("Shoot", fire_rate);
     }
 }
